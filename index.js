000877522bb2f9f5819f46efa55216bb0261d509
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-09-09 17:59:18 +0600

    Added API for Manage Products, Update Products and Delete Products

--- index.js
+++ index.js
@@ -45,10 +45,11 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('ecommerceDB').collection('users');
         const productsCollection = client.db('ecommerceDB').collection('products');
+        const cartCollection = client.db('ecommerceDB').collection('cart');
 
 
         // JWT Post
@@ -178,6 +179,134 @@ async function run() {
                 res.status(500).json({ error: "An internal server error occurred." });
             }
         });
+
+        // Update a product by ID
+        app.put('/products/:id', async (req, res) => {
+            try {
+                const { id } = req.params;
+                const updatedProduct = req.body;
+
+                // Validate the request body (you can add more validation as needed)
+                if (!updatedProduct.name || !updatedProduct.price || !updatedProduct.description) {
+                    return res.status(400).json({ message: 'Invalid product data' });
+                }
+
+                const filter = { _id: new ObjectId(id) };
+                const updateDoc = {
+                    $set: updatedProduct,
+                };
+
+                const result = await productsCollection.updateOne(filter, updateDoc);
+
+                if (result.modifiedCount === 1) {
+                    return res.status(200).json({ message: 'Product updated successfully' });
+                } else {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+            } catch (error) {
+                console.error('Error updating product:', error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+
+
+        // Delete a product by ID
+        app.delete('/products/:id', async (req, res) => {
+            try {
+                const { id } = req.params;
+
+                const filter = { _id: new ObjectId(id) };
+
+                const result = await productsCollection.deleteOne(filter);
+
+                if (result.deletedCount === 1) {
+                    return res.status(200).json({ message: 'Product deleted successfully' });
+                } else {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+            } catch (error) {
+                console.error('Error deleting product:', error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+
+
+
+        // Endpoint to add a product to the cart (without JWT authentication)
+        app.post('/cart/add/:productId', async (req, res) => {
+            try {
+                const { productId } = req.params;
+                const { email, quantity } = req.body; // Assuming email and quantity are sent in the request body
+
+                // Convert quantity to a floating-point number
+                const quantityFloat = parseFloat(quantity);
+
+                if (isNaN(quantityFloat) || quantityFloat <= 0) {
+                    return res.status(400).json({ message: 'Invalid quantity' });
+                }
+
+                // Fetch product details from the products collection
+                const product = await productsCollection.findOne({ _id: new ObjectId(productId) });
+
+                if (!product) {
+                    return res.status(404).json({ message: 'Product not found' });
+                }
+
+                // Add the product and cart item to the cart collection in MongoDB
+                const cartItem = {
+                    productId: new ObjectId(productId),
+                    product,
+                    email,
+                    quantity: quantityFloat,
+                };
+
+                await cartCollection.insertOne(cartItem);
+                return res.status(200).json({ message: 'Product added to cart successfully' });
+            } catch (error) {
+                console.error('Error adding product to cart:', error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+
+        // Endpoint to get the user's cart items
+        app.get('/cart/:email', async (req, res) => {
+            try {
+                const { email } = req.params;
+
+                // Find all cart items associated with the user's email
+                const userCart = await cartCollection.find({ email }).toArray();
+                return res.status(200).json(userCart);
+            } catch (error) {
+                console.error('Error fetching cart items:', error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+        // Add this route to your Express app
+        app.delete('/cart/delete/:itemId', async (req, res) => {
+            const { itemId } = req.params;
+
+            try {
+                const deleteResult = await cartCollection.deleteOne({ _id: new ObjectId(itemId) });
+
+                if (deleteResult.deletedCount === 1) {
+                    // Item successfully deleted
+                    return res.status(200).json({ message: 'Item deleted from cart' });
+                } else {
+                    // Item not found or not deleted
+                    return res.status(404).json({ message: 'Item not found' });
+                }
+            } catch (error) {
+                console.error('Error deleting item from cart:', error);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+
+
 
 
 
